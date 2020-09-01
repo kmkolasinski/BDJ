76d06cff863da3174e26be484f80da8499ab892f
kkol <krzysztof.kolasinski@shelfwise.ai>
2020-09-01 22:13:59 +0200

    Test PG

--- BDJ/settings.py
+++ BDJ/settings.py
@@ -81,23 +81,31 @@ WSGI_APPLICATION = 'BDJ.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 # if os.environ.get("HEROKU"):
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-# else:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.postgresql_psycopg2",
-#             "NAME": os.environ["POSTGRES_DB"],
-#             "USER": os.environ["POSTGRES_USER"],
-#             "PASSWORD": os.environ["POSTGRES_PASSWORD"],
-#             "HOST": os.environ["POSTGRES_HOST"],
-#             "PORT": os.environ["POSTGRES_PORT"],
-#         }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
+# }
+# else:
+if os.environ.get("HEROKU"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "DATABASE_URL": os.environ["DATABASE_URL"],
+        }
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": os.environ["POSTGRES_DB"],
+            "USER": os.environ["POSTGRES_USER"],
+            "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+            "HOST": os.environ["POSTGRES_HOST"],
+            "PORT": os.environ["POSTGRES_PORT"],
+        }
+    }
 
 
 # Password validation
